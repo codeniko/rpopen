ec74b7dd30f843d0f5e7d794cdf3bf12893b4350
Nikolay Feldman <nf139@cs.rutgers.edu>
2014-09-19 16:24:01 -0400

    init

--- rpserver.c
+++ rpserver.c
@@ -2,6 +2,18 @@
 	rpserver: remote popen server
 */
 
+#define DEFAULT_PORT 12345
+
+
 main(int argc, char **argv)
 {
+	int port = DEFAULT_PORT;
+	int flags, opt;
+	while ((opt = getopt(argc, argv, "p")) != 1) {
+		switch(opt) {
+			case 'p': port = atoi(optarg); break;
+			case '?':
+			default:	
+		}
+	}
 }
